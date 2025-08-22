e88a05bfe01f1d41e8534faea9bdc8aa4866efc4
Harisankar <harisankar@uniqnex360.com>
2025-08-22 17:38:54 +0530

    celery issue resolved

--- ecommerce_tool/ecommerce_tool/celery.py
+++ ecommerce_tool/ecommerce_tool/celery.py
@@ -1,14 +1,14 @@
-# import os
-# from celery import Celery
+import os
+from celery import Celery
 
 # # Set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_tool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_tool.settings')
 
-# app = Celery(
-#     'ecommerce_tool',
-#     broker='redis://:foobaredUniqnex@localhost:6379/0',
-#     backend='redis://:foobaredUniqnex@localhost:6379/0',
-# )
+app = Celery(
+    'ecommerce_tool',
+    broker='redis://:foobaredUniqnex@localhost:6379/0',
+    backend='redis://:foobaredUniqnex@localhost:6379/0',
+)
 
 # # Load task modules from Django settings
 # app.config_from_object('django.conf:settings', namespace='CELERY')
