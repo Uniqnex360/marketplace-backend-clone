15f128c65f01cfd9f0ffa68397b746170075378c
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-06-02 17:38:14 +0530

    debug statement

--- ecommerce_tool/clickhouse/config.py
+++ ecommerce_tool/clickhouse/config.py
@@ -1,6 +1,7 @@
 import clickhouse_connect
 from django.conf import settings
 
+print(settings.CLICKHOUSE_HOST, "host")
 client = clickhouse_connect.get_client(
     host=settings.CLICKHOUSE_HOST,
     port=int(settings.CLICKHOUSE_PORT or 8123),
